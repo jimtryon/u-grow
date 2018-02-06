8a1d4fa058bb563b76518d9d13cb1505e31ba44a
Jim Tryon <tryonjf@gmail.com>
2018-02-06 04:00:23 -0800

    Rename package

--- SpringBoot/src/main/java/com/ugrow/SpringBoot/Application.java
+++ SpringBoot/src/main/java/com/ugrow/SpringBoot/Application.java
@@ -1,4 +1,4 @@
-package com.ugrow.hello;
+package com.ugrow.SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- SpringBoot/src/main/java/com/ugrow/SpringBoot/HelloController.java
+++ SpringBoot/src/main/java/com/ugrow/SpringBoot/HelloController.java
@@ -1,4 +1,4 @@
-package com.ugrow.hello;
+package com.ugrow.SpringBoot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
